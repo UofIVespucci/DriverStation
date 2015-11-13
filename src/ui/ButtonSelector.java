c499f83abf04b6aae6e7404ad011fe7228f3c580
well3112 <lance.a.wells@gmail.com>
2015-11-13 10:31:39 -0800

    Implemented StackPanes for text transitions on toggle buttons

--- src/ui/ButtonSelector.java
+++ src/ui/ButtonSelector.java
@@ -7,15 +7,18 @@ import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.text.Font;
-
-import java.util.Collection;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 
 public class ButtonSelector extends HBox{
     private static ToggleButton cameraButton;
     private static ToggleButton debugButton;
     private static ToggleButton mapButton;
     private static ToggleGroup toggleGroup;
+    private final Region rect = new Region();
+    private static StackPane cameraStack;
+    private static StackPane debugStack;
+    private static StackPane mapStack;
 
     public ButtonSelector()
     {
@@ -24,17 +27,35 @@ public class ButtonSelector extends HBox{
         mapButton = new ToggleButton();
         toggleGroup = new ToggleGroup();
 
+        cameraStack = new StackPane();
+        debugStack = new StackPane();
+        mapStack = new StackPane();
+
+//        cameraStack.getChildren().addAll(cameraButton, rect);
+        cameraStack.getChildren().add(cameraButton);
+        debugStack.getChildren().add(debugButton);
+        mapStack.getChildren().add(mapButton);
+
+//        rect.setArcHeight(0);
+//        rect.setArcWidth(0);
+//        rect.setFill(Color.RED);
+        rect.getStyleClass().add("carbon-button");
+
         cameraButton.getStyleClass().add("toggle-camera");
         mapButton.getStyleClass().add("toggle-map");
         debugButton.getStyleClass().add("toggle-debug");
-        getChildren().addAll(cameraButton, mapButton, debugButton);
+        getChildren().addAll(cameraStack, mapStack, debugStack);
 
-        setHgrow(cameraButton, Priority.ALWAYS);
-        setHgrow(mapButton, Priority.ALWAYS);
-        setHgrow(debugButton, Priority.ALWAYS);
+        setHgrow(cameraStack, Priority.ALWAYS);
+        setHgrow(mapStack, Priority.ALWAYS);
+        setHgrow(debugStack, Priority.ALWAYS);
 
         setMaxWidth(Double.MAX_VALUE);
         setAlignment(Pos.TOP_CENTER);
+
+        cameraStack.setMaxWidth(Double.MAX_VALUE);
+        mapStack.setMaxWidth(Double.MAX_VALUE);
+        debugStack.setMaxWidth(Double.MAX_VALUE);
 
         cameraButton.setMaxWidth(Double.MAX_VALUE);
         mapButton.setMaxWidth(Double.MAX_VALUE);
@@ -48,6 +69,7 @@ public class ButtonSelector extends HBox{
         cameraButton.setToggleGroup(toggleGroup);
         mapButton.setToggleGroup(toggleGroup);
         debugButton.setToggleGroup(toggleGroup);
+
 
 //        showText(true);
         setPadding(new Insets(0, 0, 0, 0));
