dabc1aa1156d6d3434fd8964ede1b5ce8bc1485f
Brett Menzies <scimor5@gmail.com>
2016-02-10 16:11:32 -0800

    Robot code utilizes new Qik library

--- RobotCode/RobotCode.ino
+++ RobotCode/RobotCode.ino
@@ -3,16 +3,21 @@
 #include "serial/Decoder.h"
 #include "VespuChat.h"
 
+#include "Qik/Qik.h"
+
 #include <AltSoftSerial.h>
 AltSoftSerial motorSerial;
+
+Qik motor(&motorSerial);
 
 extern VespuChat vespuChat;
 MotorCommand mcmd([](uint8_t left, uint8_t right){
     MotorCommand::build(vespuChat, left, right); //echo command
-    motorSerial.write(0x89);//motor 0 speed set
-    motorSerial.write(left&0xff);
-    motorSerial.write(0x8D);//motor 1 speed set
-    motorSerial.write(right&0xff);
+    motor.setMotors(left, right);
+    //motorSerial.write(0x89);//motor 0 speed set
+    //motorSerial.write(left&0xff);
+    //motorSerial.write(0x8D);//motor 1 speed set
+    //motorSerial.write(right&0xff);
 });
 Error ecmd([](uint8_t code){
     Error::build(vespuChat, code);
@@ -24,7 +29,8 @@ void setup(){
     Serial.begin(9600);
     motorSerial.begin(38400);
     delay(100);
-    motorSerial.write(0xAA);//motor BAUD detection byte
+    motor.enable();
+    //motorSerial.write(0xAA);//motor BAUD detection byte
     Error::build(vespuChat, 0x00/*starting*/);
 }
 
